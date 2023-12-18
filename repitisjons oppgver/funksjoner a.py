6b2ad7a2166502bf1f696c3d77bf59d66c5d7639
danieldim2007 <153490221+danieldim2007@users.noreply.github.com>
2023-12-18 19:27:37 +0100

    Add files via upload

---
+++ repitisjons oppgver/funksjoner a.py
@@ -0,0 +1,6 @@
+def summeringAvTall():
+    førsteTall = int(input('Gi meg et tall'))
+    andreTall = int(input('Gi meg et nytt tall'))
+    sum = førsteTall + andreTall
+    print(sum)
+summeringAvTall()
---
+++ repitisjons oppgver/funksjoner b.py
@@ -0,0 +1,14 @@
+
+
+def listeMedTall():
+    førsteTall = int(input('Gi meg et tall'))
+    andreTall = int(input('Gi meg et nytt tall'))
+    tredjeTall = int(input('Gi meg et nytt tall'))
+    fjerdeTall = int(input('Gi meg et nytt tall'))
+    femteTall = int(input('Gi meg et nytt tall'))
+    
+    sum = førsteTall + andreTall + tredjeTall + fjerdeTall + femteTall
+    gjennomsnittet = sum/5
+    print(gjennomsnittet)
+    
+listeMedTall()
